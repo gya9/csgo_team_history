51f4ba40b6fd548e0d22f9ae4725ce66eea3bd8b
高先修平 <gya9@takasakishuuheinoMacBook-Air.local>
2019-05-21 01:13:13 +0900

    modified index

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -48,9 +48,13 @@ module.exports = {
     index: path.resolve(__dirname, '../../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../dist'),
+    // assetsRoot: path.resolve(__dirname, '../../dist'),
+    // assetsSubDirectory: 'static',
+    // assetsPublicPath: '/',
+
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
