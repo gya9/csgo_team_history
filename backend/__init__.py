35d23a2e76768d28ef851d5db10cb480cab3c6d5
高先修平 <gya9@takasakishuuheinoMacBook-Air.local>
2019-05-21 17:32:29 +0900

    modify for gce

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py（←application.py）
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask('FLASK-VUE',
--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from random import *
 import pandas as pd
 import numpy as np
@@ -17,15 +17,20 @@ def random_number():
     }
     return jsonify(response)
 
-@api.route('/df/<string:teamname>')
-def df_data(teamname):
+@api.route('/df/teamlist')
+def team_list():
+    df = pd.read_csv('backend/team_history.csv', index_col=0, parse_dates=True)
+    
+
+@api.route('/df/<string:search_word>')
+def df_data(search_word):
     import datetime as dt
     df = pd.read_csv('backend/team_history.csv', index_col=0, parse_dates=True)
     tmp = []
     response = {}
 
     for date in df.index:
-        tmp2 = list(df.columns[df.loc[date] == teamname])
+        tmp2 = list(df.columns[df.loc[date] == search_word.replace(' ','_')])
         if tmp2 != tmp and len(tmp2) == 5:
             tmp = tmp2
 
